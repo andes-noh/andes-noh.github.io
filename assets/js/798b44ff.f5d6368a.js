01c7de49f19b3c1cac6af1ccf3e6f3922cccedd1
andes-noh <nkj9693@gmail.com>
2022-11-10 08:34:08 +0000

    deploy: 28282d1f07bc89d639ef03e266d9f6a44ccd194c

---
+++ assets/js/798b44ff.f5d6368a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog_template=self.webpackChunkblog_template||[]).push([[3745],{7875:e=>{e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/eks","name":"eks","count":1,"permalink":"/blog/tags/eks"}')}}]);
---
+++ assets/js/b0f1c099.a1e1cbb4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog_template=self.webpackChunkblog_template||[]).push([[9387],{6462:e=>{e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/kubernetes","name":"kubernetes","count":6,"permalink":"/blog/tags/kubernetes"}')}}]);
